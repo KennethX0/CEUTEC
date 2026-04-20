548196c39849d511e93fa6f5128699bac15e3a95
KennethX0 <141522449+KennethX0@users.noreply.github.com>
2026-04-19 23:42:44 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+from Backend.app import app
+import os
+
+
+if __name__ == '__main__':
+    host = os.getenv('FLASK_HOST', '0.0.0.0')
+    port = int(os.getenv('PORT', '5000'))
+    debug = os.getenv('FLASK_DEBUG', '0') == '1'
+    app.run(host=host, port=port, debug=debug)
